ff44b05d5a98c9791768ac50748ed81e175e93bc
Job Etokabeka <jobet@MacBook-Air.local>
2025-05-26 10:32:44 +0100

    Mise à jour du projet Django : corrections + nouvelles fonctionnalités

---
+++ core/core/management/commands/create_superuser.py
@@ -0,0 +1,19 @@
+# core/management/commands/create_superuser.py
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+class Command(BaseCommand):
+    help = 'Crée un superutilisateur automatiquement'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@gmail.com',
+                password='admin'
+            )
+            self.stdout.write(self.style.SUCCESS('Superutilisateur créé'))
+        else:
+            self.stdout.write('Superutilisateur existe déjà')
