1a332f0717a5cf62beae20a93dc14dc6f9f18a7f
Job Etokabeka <jobet@MacBook-Air.local>
2025-05-26 21:19:57 +0100

    Mise à jour du projet Django : corrections + nouvelles fonctionnalités

--- core/admin.py
+++ core/admin.py
@@ -9,3 +9,7 @@ admin.site.register(TypeAbonnement)
 admin.site.register(Paiement)
 admin.site.register(Diagnostic)
 admin.site.register(Protocole)
+
+admin.site.site_header = "AM Consulting"
+admin.site.site_title = "AMC - Administration"
+admin.site.index_title = "Tableau de bord de AMC"
